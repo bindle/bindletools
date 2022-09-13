fe23e5dcb993f08ed410c5bb57b0e0af3222f3f9
David M. Syzdek <david@syzdek.net>
2022-09-12 22:54:13 -0800

    libbindle: adding error code to bindle_hex_decode()

--- lib/libbindle/lencoding.c
+++ lib/libbindle/lencoding.c
@@ -822,7 +822,10 @@ bindle_hex_decode(
 
    // verifies encoded data contains only valid characters
    if ((rc = bindle_base64_verify(map, (const char *)src, n)) == -1)
+   {
+      errno = EBADMSG;
       return(-1);
+   };
    if ( (rc > (ssize_t)s) && ((dst)) )
    {
       errno = ENOBUFS;
